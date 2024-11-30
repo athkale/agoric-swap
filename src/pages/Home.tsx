510324b9e4de25e7d6ee5d73a88eebccd79e2df2
vinay4656 <vinayvinnu2646@gmail.com>
2024-11-30 15:04:00 +0530

    header

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Hero from '@/components/Home/Hero'
+import Header from '@/layout/Header'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+   <>
+   <Header />
+   <Hero /> 
+   </>
+  )
+}
+
+export default HomePage
